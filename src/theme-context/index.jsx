b2ea1ff431cb6ecb77273e3ccd87206b1f3844ad
José Victor <jose08.victor@gmail.com>
2024-01-07 17:58:01 -0300

    update theme-context

--- src/theme-context/index.jsx
+++ src/theme-context/index.jsx
@@ -1,7 +1,7 @@
 import { createContext, useState } from "react";
 
 export const themes = {
-    colors: ['rgb(255, 255, 0)', 'rgb(0, 255, 255)', 'rgb(255, 0, 0)', 'rgb(0, 255, 0)', 'rgb(255, 100, 0)', 'rgb(255,0,181)', 'rgb(0,94,255)', 'rgb(0,255,132)', 'rgb(128,0,255)', 'rgb(255,255,255)', 'rgb(165,255,0)'],
+    colors: ['rgb(255, 255, 0)', 'rgb(0, 255, 255)', 'rgb(255, 0, 0)', 'rgb(0, 255, 0)', 'rgb(255, 100, 0)', 'rgb(255,0,181)', 'rgb(0,94,255)', 'rgb(0,255,132)', 'rgb(148,0,291)', 'rgb(255,255,255)', 'rgb(165,255,0)'],
     index: Math.floor(Math.random() * 11)
 }
 
